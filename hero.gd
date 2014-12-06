b04e3002a9fbb4832015de872bdd20e2441b50ef
Melnikov Sergey <me@rnr.name>
2014-12-06 17:42:44 +0600

    Change algorithm enemy moving.

--- hero.gd
+++ hero.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
-var speed = 3.0
 
+var speed = 3.0
+var lastStep = Vector2(0, 0)
 func _ready():
 	set_fixed_process(true)
 	pass
@@ -9,18 +10,24 @@ func getSpeed():
 	return speed
 	
 func getAngle():
-	var pos1 = get_node("Position2D").get_pos()
-	var pos2 = get_pos()
-	var pointTo = pos1 + pos2
 	var mousePoint = Input.get_mouse_pos()
+	var pointTo = getCurrentPoint()
 
 	return pointTo.angle_to_point(mousePoint)
 	
 func rotate():
 	set_rot(getAngle())
+	
+func getCurrentPoint():
+	var pos1 = get_node("Position2D").get_pos()
+	var pos2 = get_pos()
+
+	return pos1 + pos2
+
+func getLastStep():
+	return lastStep
 
 func _fixed_process(delta):
-
 	var move_left = Input.is_action_pressed("ui_left")
 	var move_right = Input.is_action_pressed("ui_right")
 	var move_up = Input.is_action_pressed("ui_up")
@@ -29,6 +36,7 @@ func _fixed_process(delta):
 	var dx = (move_left * -1) + (move_right * 1)
 	var dy = (move_up * -1) + (move_down * 1)
 
-	move(speed * Vector2(dx, dy))
+	lastStep = speed * Vector2(dx, dy)
+	move(lastStep)
 
 	rotate()
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,18 +1,18 @@
 extends KinematicBody2D
 
-var _hero
+var hero
 var speed
 
 func _ready():
 	speed = 2.0 * randf()
 	set_fixed_process(true)
 	
-func setHero(hero):
-	_hero = hero
+func setHero(h):
+	hero = h
 	
 func _fixed_process(delta):
 	var point = get_pos()
-	var heroPoint = _hero.get_pos()
+	var heroPoint = hero.get_pos() + hero.getLastStep()
 	var moveVect = (heroPoint - point).normalized()
 	var angle = point.angle_to_point(heroPoint)
 	set_rot(angle)
