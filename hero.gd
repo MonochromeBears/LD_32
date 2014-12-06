a7dc12b1923de33b4f69110c7e0a0a81064a776a
Lancer411 <skidivay@gmail.com>
2014-12-06 14:54:46 +0600

    New textures. Hero rotation.
    Hero texture.
    Enemy texture.
    Hero rotation.

--- hero.gd
+++ hero.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 func _ready():
 	set_fixed_process(true)
+	pass
 	
-func rotate():
-	var point = get_pos()
+func rotate(pointTo):
+	
 	var mousePoint = Input.get_mouse_pos()
-	
-	set_rot(point.angle_to_point(mousePoint))
+	set_rot(pointTo.angle_to_point(mousePoint))
 
 func _fixed_process(delta):
 	var move_left = Input.is_action_pressed("ui_left")
@@ -16,4 +16,8 @@ func _fixed_process(delta):
 	var dx = (move_left * -1) + (move_right * 1)
 	var dy = (move_up * -1) + (move_down * 1)
 	move(Vector2(dx,dy))
-	rotate()
+	var pos1 = get_node("Position2D").get_pos()
+	var pos2 = get_pos()
+	pos2.x += pos1.x
+	pos2.y += pos1.y
+	rotate(pos2)
