0f568f83b31a7c7543a45d5eb354e2715cd9ff56
Melnikov Sergey <me@rnr.name>
2014-12-06 13:03:45 +0600

    Add rotating.

--- hero.gd
+++ hero.gd
@@ -2,6 +2,12 @@ extends KinematicBody2D
 func _ready():
 	set_fixed_process(true)
 	pass
+	
+func rotate():
+	var point = get_pos()
+	var mousePoint = Input.get_mouse_pos()
+	
+	set_rot(point.angle_to_point(mousePoint))
 
 func _fixed_process(delta):
 	var move_left = Input.is_action_pressed("ui_left")
@@ -11,3 +17,4 @@ func _fixed_process(delta):
 	var dx = (move_left * -1) + (move_right * 1)
 	var dy = (move_up * -1) + (move_down * 1)
 	move(Vector2(dx,dy))
+	rotate()
