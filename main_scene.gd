bbe334352cadb4e31b69f456ec9fb2b39262b923
Maxim Sergeev <sergeev@a2design.biz>
2014-12-07 00:17:55 +0600

    Added: score text

--- main_scene.gd
+++ main_scene.gd
@@ -4,6 +4,7 @@ var Hero = preload("hero.xml")
 var Enemy = preload("enemy.xml")
 var hero
 var health_bar
+var score_label
 
 var scores = 0
 
@@ -11,6 +12,7 @@ func _ready():
 	hero = Hero.instance()
 	hero.set_pos(Vector2(500, 500))
 	health_bar = get_node("health")
+	score_label = get_node("score")
 	add_child(hero)
 	for i in range(1):
 		var enemy = Enemy.instance()
@@ -23,6 +25,7 @@ func _ready():
 func killEnemy(enemy):
 	scores += enemy.getScores()
 	print("Scores: ", scores)
+	score_label.set_text("SCORE: " + str(scores))
 	self.remove_and_delete_child(enemy)
 	
 
