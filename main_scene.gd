45eaf1b97c683c4a17c63662f9d6c893447fecc6
Lancer411 <skidivay@gmail.com>
2014-12-07 15:32:53 +0600

    Enemy spawn fixes

--- main_scene.gd
+++ main_scene.gd
@@ -6,6 +6,7 @@ var Boss = preload("booboss.xml")
 var deadBody = preload("DeadEnemy.xml")
 
 var bossCounter = 0
+var enemyCounter = 0
 var hero
 var health_bar
 var score_label
@@ -17,6 +18,7 @@ var death_sound
 
 func _ready():
 	randomize()
+	enemyCounter = 0
 	get_node("Timer").start()
 
 	health_bar = get_node("health")
@@ -39,21 +41,38 @@ func getEnemyHealth():
 	return exp(scores / 100000.0 + 1) / 4
 	
 func getEnemySpeed():
+	var speed = scores / 100000.0 + 0.5
+	get_node("Timer").set_wait_time(1 / (speed))
 	return scores / 100000.0 + 0.5
-	
+
+func newCord():
+	var vert = (randf() > 0.5)
+	var hori = (randf() > 0.5)
+	var HoriPos = viewbox[0] * randf() * vert
+	var VertPos = viewbox[1] * randf() * (!vert)
+	if hori:
+		if HoriPos:
+			VertPos = viewbox[1]
+		else:
+			HoriPos = viewbox[0]
+	return Vector2(HoriPos, VertPos )
 func createEnemy():
-	var pos = Vector2(viewbox[0] * randf(), viewbox[1] * randf())
-	var enemy = Enemy.instance()
-	
-	enemy.setHealth(getEnemyHealth())
-	enemy.setSpeed(getEnemySpeed())
-	enemy.setHero(hero)
-	enemy.set_pos(pos)
-	add_child(enemy)
-	print("Creating enemy in (", pos.x, ", ", pos.y,")")
+	if(enemyCounter < 75):
+		
+		var pos = newCord()
+		
+		var enemy = Enemy.instance()
+		
+		enemy.setHealth(getEnemyHealth())
+		enemy.setSpeed(getEnemySpeed())
+		enemy.setHero(hero)
+		enemy.set_pos(pos)
+		add_child(enemy)
+		enemyCounter += 1
+		print("Creating enemy in (", pos.x, ", ", pos.y,")")
 	
 func createBoss():
-	var pos = Vector2(viewbox[0] * randf(), viewbox[1] * randf())
+	var pos = newCord()
 	var enemy = Boss.instance()
 
 	enemy.setHealth(5 * getEnemyHealth())
@@ -80,6 +99,8 @@ func killEnemy(enemy):
 	Decal.set_rot(enemy.get_rot())
 	get_child("deadBodies").add_child(Decal)
 	enemy.queue_free()
+	if(enemy extends preload("res://scripts/enemy.gd")):
+		enemyCounter -= 1
 	death_sound.play()
 	
 
